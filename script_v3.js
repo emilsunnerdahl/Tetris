b77af8fa8e164b27d71d5cf5f85e35a4743dd7e3
Emil Sunnerdahl <emil.sunnerdahl@gmail.com>
2024-01-09 16:33:07 +0100

    fixed some last things

--- script_v3.js
+++ script_v3.js
@@ -1,6 +1,5 @@
 // Detta är mitt försök att fixa till v1 så att det funkar. Datum: 2024-01.
 
-// TODO: Visa kommande tre bitar
 // TODO: Fixa så att man inte kan gå igenom bitar i sidled
 // TODO: Fixa så man förlorar när det är fullt
 
@@ -21,11 +20,14 @@ class Piece {
     this.yDirections = yDirections;
     this.x;
     this.y;
+    this.ids;
     this.color = color;
     this.game = game;
     this.offset = offset;
+    this.xRightOffset = Math.max(...this.xDirections[0]);
 
     this.updateCoords();
+    this.updateIds();
   }
 
   updateCoords() {
@@ -41,11 +43,28 @@ class Piece {
     }
   }
 
+  updateIds() {
+    this.ids = [];
+    for (let i = 0; i < 4; i++) {
+      this.ids.push(`${this.x[i]}${this.y[i]}`);
+    }
+  }
+
+  checkForCollision(offset) {
+    this.updateIds();
+    return [0, 1, 2, 3].some(
+      (i) =>
+        this.game.getColor(this.x[i] + offset[0], this.y[i] + offset[1]) !=
+          colors.black &&
+        !this.ids.includes(`${this.x[i] + offset[0]}${this.y[i] + offset[1]}`)
+    );
+  }
+
   moveLeft() {
     if (!arrowLeft) {
       return;
     }
-    if (!this.x.some((x) => x == 0)) {
+    if (!this.x.some((x) => x == 0) && !this.checkForCollision([-1, 0])) {
       this.x = this.x.map((x) => x - 1);
     }
     if (this.offset[0] != 0) {
@@ -58,10 +77,10 @@ class Piece {
     if (!arrowRight) {
       return;
     }
-    if (!this.x.some((x) => x == 9)) {
+    if (!this.x.some((x) => x == 9) && !this.checkForCollision([1, 0])) {
       this.x = this.x.map((x) => x + 1);
     }
-    if (this.offset[0] + this.xDirections[0][3] != 9) {
+    if (this.offset[0] + this.xRightOffset != 9) {
       this.offset[0] = this.offset[0] + 1;
     }
     arrowRight = false;
@@ -104,7 +123,6 @@ class Piece {
     }
     while (!this.stoppedMoving()) {
       this.y = this.y.map((y) => y + 1);
-      // this.offset[1] = this.offset[1] + 1;
     }
 
     spaceDown = false;
@@ -131,25 +149,13 @@ class Piece {
   }
 
   stoppedMoving() {
-    if (this.y[3] == 19) {
-      return true;
-    }
+    return this.y[3] == 19 || this.checkForCollision([0, 1]);
+  }
 
-    let ownIds = [];
-    for (let i = 0; i < 4; i++) {
-      ownIds.push(`r${this.x[i]}${this.y[i]}`);
-    }
-
-    for (let i = 0; i < 4; i++) {
-      let idUnder = `r${this.x[i]}${this.y[i] + 1}`;
-      if (
-        this.game.getColor(this.x[i], this.y[i] + 1) != colors.black &&
-        !ownIds.includes(idUnder)
-      ) {
-        return true;
-      }
-    }
-    return false;
+  isGameOver() {
+    return [0, 1, 2, 3].some(
+      (i) => this.game.getColor(this.x[i], this.y[i]) != colors.black
+    );
   }
 }
 
@@ -162,6 +168,8 @@ class Game {
     this.pieces = [cube, long, sPiece, zPiece, lPiece, lMirror, tPiece];
     this.board = [];
     this.blackArray;
+    this.nextPieces = [];
+    this.gameOver = false;
   }
 
   updateBoard(x, y, color) {
@@ -181,7 +189,7 @@ class Game {
     }
   }
 
-  createDivs() {
+  createBoardDivs() {
     for (let i = 0; i < this.height; i++) {
       let arr = [];
       for (let j = 0; j < this.width; j++) {
@@ -197,18 +205,42 @@ class Game {
     }
   }
 
+  createNextPiecesDivs() {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 4; j++) {
+        let div = document.createElement("div");
+        div.setAttribute("id", `n-${j}${i}`);
+        div.setAttribute("class", "nextPieceSquare");
+        document.querySelector(".nextPieces").appendChild(div);
+      }
+    }
+  }
+
+  createNextPieces() {
+    let randomInt;
+    let pieceObject;
+
+    for (let i = 0; i < 3; i++) {
+      randomInt = Math.floor(Math.random() * 7);
+      pieceObject = this.pieces[randomInt];
+      this.nextPieces.push(pieceObject);
+    }
+  }
+
   createNewPiece() {
     let randomInt = Math.floor(Math.random() * 7);
     let pieceObject = this.pieces[randomInt];
+    this.nextPieces.push(pieceObject);
+
+    let nextPiece = this.nextPieces.shift();
     this.activePiece = new Piece(
-      pieceObject.name,
-      pieceObject.color,
-      [...pieceObject.x],
-      [...pieceObject.y],
+      nextPiece.name,
+      nextPiece.color,
+      [...nextPiece.x],
+      [...nextPiece.y],
       this,
-      [...pieceObject.offset]
+      [...nextPiece.offset]
     );
-    console.log(this.activePiece.name);
   }
 
   draw() {
@@ -220,7 +252,44 @@ class Game {
     }
   }
 
+  drawNextPieces() {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 4; j++) {
+        document.getElementById(`n-${j}${i}`).style.backgroundColor =
+          colors.black;
+      }
+    }
+
+    let nextPieceX;
+    let nextPieceY;
+    for (let i = 0; i < 3; i++) {
+      nextPieceX = this.nextPieces[i].x[0];
+      nextPieceY = this.nextPieces[i].y[0];
+      for (let j = 0; j < 4; j++) {
+        document.getElementById(
+          `n-${nextPieceX[j] + this.nextPieces[i].offset[0] - 3}${
+            nextPieceY[j] + 3 * i
+          }`
+        ).style.backgroundColor = this.nextPieces[i].color;
+      }
+    }
+  }
+
+  checkForGameOver() {
+    if (this.activePiece.isGameOver()) {
+      this.gameOver = true;
+      let header = document.getElementById("pause");
+      header.style.visibility = "visible";
+      header.textContent = "Game Over";
+      this.activePiece.updateGameBoard();
+    }
+  }
+
   gameLoop() {
+    if (this.gameOver) {
+      return;
+    }
+
     if (pause) {
       return;
     }
@@ -230,6 +299,8 @@ class Game {
     if (this.activePiece.stoppedMoving()) {
       this.removeFullColumn();
       this.createNewPiece();
+      this.drawNextPieces();
+      this.checkForGameOver();
     }
 
     this.draw();
@@ -237,8 +308,11 @@ class Game {
   }
 
   start() {
-    this.createDivs();
+    this.createBoardDivs();
+    this.createNextPiecesDivs();
+    this.createNextPieces();
     this.createNewPiece();
+    this.drawNextPieces();
     let gameInterval = setInterval(() => {
       this.gameLoop();
     }, 1000 / this.fps);
@@ -285,12 +359,14 @@ window.addEventListener("keyup", (event) => {
 const colors = {
   yellow: "#ffcc00",
   lightBlue: "#33ccff",
-  green: "green",
+  green: "#019429",
   red: "#e60000",
   orange: "#ff6600",
   darkBlue: "#0052cc",
-  purple: "#6600ff",
+  purple: "#7c029e",
   black: "#0C0D0E",
+  grey: "#484848",
+  lightGrey: "#989898",
 };
 
 const cube = {
